eb20edcad0ee4b5ae735ac87da3c889caa5808f5
Julio-Valderrama <j.valdemartinez@gmail.com>
2022-11-19 11:43:09 +0900

    package.json modified for db-migrations

--- src/database.ts
+++ src/database.ts
@@ -7,7 +7,7 @@ const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_T
 
 const client = new Pool({
   host: POSTGRES_HOST,
-  database: ENV === 'test' ? POSTGRES_TEST_DB : POSTGRES_DB,
+  database: POSTGRES_DB,
   user: POSTGRES_USER,
   password: POSTGRES_PASSWORD
 });
