4c48615907f5e313a3ea88c9a5a49e0e6745aaa8
Julio-Valderrama <j.valdemartinez@gmail.com>
2022-11-22 23:56:53 +0900

    getBalanceinTotalCategory DONE

--- src/services/appQueriesController.ts
+++ src/services/appQueriesController.ts
@@ -18,11 +18,29 @@ const getBalanceInTotal = async (req: Request, res: Response) => {
   }
 };
 
+// To show all the EXPENSES or INCOME from a CATEGORY (Category when clicking and Account provided) FOR THE TOTAL
+// INCLUDED IN TOTAL FUNCTION
+
+const getBalanceinTotalCategory = async (req: Request, res: Response) => {
+  try {
+    const result = await store.getBalanceinTotalCategory(
+      req.params.category_name,
+      req.params.category_type_name
+    );
+    console.log(result);
+    console.log(req.params.category_type_name);
+    res.status(200).json(result);
+  } catch (err) {
+    const formatedError = formatError(err);
+    res.status(400).json(formatedError);
+  }
+};
+
 // Get the total Balance per account when name provided
 
-const getBalancePerAccount = async (req: Request, res: Response) => {
+const getTotalBalancePerAccount = async (req: Request, res: Response) => {
   try {
-    const result = await store.getBalancePerAccount(req.params.account_name, req.params.category_type);
+    const result = await store.getTotalBalancePerAccount(req.params.account_name, req.params.category_type);
     console.log(result);
     res.status(200).json(result);
   } catch (err) {
@@ -33,10 +51,13 @@ const getBalancePerAccount = async (req: Request, res: Response) => {
 
 const appQueriesRoutes = (app: express.Application) => {
   // Get the total of all Accounts included in Total per EXPENSES or INCOME
-  app.get('/api/get-balance-in-total/:category_type_name', getBalanceInTotal);
+  app.get('/api/balance-in-total/:category_type_name', getBalanceInTotal);
+
+  // To show all the EXPENSES or INCOME from a CATEGORY (Category when clicking and Account provided) FOR THE TOTAL
+  app.get('/api/balance-in-total-category/:category_name/:category_type_name', getBalanceinTotalCategory);
 
   // Get the total Balance per account when name provided per EXPENSES or INCOME
-  app.get('/api/get-balance/:account_name/:category_type', getBalancePerAccount);
+  app.get('/api/total-balance/:account_name/:category_type', getTotalBalancePerAccount);
 };
 
 export default appQueriesRoutes;
--- src/services/appQueriesModel.ts
+++ src/services/appQueriesModel.ts
@@ -2,10 +2,10 @@ import client from '../database/database';
 
 export class AppQueries {
   //  To show all the EXPENSES or INCOME from all the accounts included in TOTAL in the HOME PAGE
+  // INCLUDED IN TOTAL FUNCTION
 
-  async getBalanceInTotal(categoryTypeName: string) {
+  async getBalanceInTotal(categoryTypeName: string): Promise<string[]> {
     try {
-      console.log(categoryTypeName);
       const connection = await client.connect();
       const sql =
         "SELECT category.name, category_type.name as type, SUM(account.amount_default_currency) FROM account INNER JOIN accounts ON account.accounts_id = accounts.id INNER JOIN category ON account.category_id = category.id INNER JOIN category_type ON category.category_type_id = category_type.id WHERE accounts.included_total = 'yes' AND category_type.name = ($1) GROUP BY category.name, category_type.name;";
@@ -17,9 +17,25 @@ export class AppQueries {
     }
   }
 
-  // Get the total Balance per account when name provided
+  // To show all the EXPENSES or INCOME from a CATEGORY (Category when clicking and Account provided) FOR THE TOTAL
+  // INCLUDED IN TOTAL FUNCTION
 
-  async getBalancePerAccount(accountName: string, category_type_name: string) {
+  async getBalanceinTotalCategory(categoryName: string, categoryTypeName: string) {
+    try {
+      const connection = await client.connect();
+      const sql =
+        "SELECT account.date, accounts.name, category.name, account.amount_account_currency, account.amount_default_currency, account.comment FROM account INNER JOIN accounts ON account.accounts_id = accounts.id INNER JOIN category ON account.category_id = category.id INNER JOIN category_type ON category.category_type_id = category_type.id WHERE category.name = ($1) AND category_type.name = ($2) AND accounts.included_total = 'yes';";
+      const result = await connection.query(sql, [categoryName, categoryTypeName]);
+      console.log(result.rows);
+      return result.rows;
+    } catch (err) {
+      throw new Error(`Could not get the Balance for the Account. Error: ${err}`);
+    }
+  }
+
+  // Get the total Balance per account when name provided - NO TOTAL
+
+  async getTotalBalancePerAccount(accountName: string, category_type_name: string): Promise<string[]> {
     try {
       const connection = await client.connect();
       const sql =
@@ -28,14 +44,21 @@ export class AppQueries {
       connection.release();
       return result.rows;
     } catch (err) {
-      throw new Error(`Could not get the Balance for the Account. Error: ${err}`);
+      throw new Error(`Could not get the TOTAL Balance for the Account. Error: ${err}`);
     }
   }
 
   // To show all the EXPENSES or INCOME from a CATEGORY (Category when clicking and Account provided)
-
-  //async getBalancePerCategoryAccount(categoryName: string, )
-
-  //  To show all the ACCOUNTS in the Accounts page from the menu
-  //  - INDEX GET =
+  // NO TOTAL
+  async getBalancePerAccountCategory(account_name: string, category_type_id: string) {
+    try {
+      const connection = await client.connect();
+      const sql =
+        'SELECT account.date, accounts.name, category.name , account.amount_account_currency, account.amount_default_currency, account.comment FROM account INNER JOIN accounts ON account.accounts_id = accounts.id INNER JOIN category ON account.category_id = category.id INNER JOIN category_type ON category.category_type_id = category_type.id WHERE accounts.name = ($1) AND category_type.name =(&2)';
+      const result = await connection.query(sql, [account_name, category_type_id]);
+      return result.rows;
+    } catch (err) {
+      throw new Error(`Could not get the Balance for the Account. Error: ${err}`);
+    }
+  }
 }
