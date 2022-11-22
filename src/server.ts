e795748cc273d13690ba80d03862492015d80d2d
Julio-Valderrama <j.valdemartinez@gmail.com>
2022-11-22 09:48:51 +0900

    Hnadlers folder changed to controllers

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 
-import usersRoutes from './handlers/users';
-import categoryTypeRoutes from './handlers/category_types';
-import categoryRoutes from './handlers/categories';
-import currencyRoutes from './handlers/currencies';
-import accountsRoutes from './handlers/accounts';
-import accountRoutes from './handlers/account';
+import usersRoutes from './controllers/users';
+import categoryTypeRoutes from './controllers/category_types';
+import categoryRoutes from './controllers/categories';
+import currencyRoutes from './controllers/currencies';
+import accountsRoutes from './controllers/accounts';
+import accountRoutes from './controllers/account';
 
 const app: express.Application = express();
 
