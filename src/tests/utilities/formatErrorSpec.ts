bdf6b8e73933a5cee10a0ae247331e8ec63dccbb
Julio-Valderrama <j.valdemartinez@gmail.com>
2022-11-22 09:55:50 +0900

    formatError Unit Test DONE

---
+++ src/tests/utilities/formatErrorSpec.ts
@@ -0,0 +1,9 @@
+import formatError from '../../utilities/formatError';
+
+describe('<<<<<< Testing /Utilities FORMATERROR function >>>>>>', () => {
+  it('Should create an ERROR to an instances of an Error with .message', async () => {
+    const myError = new Error('This is the ERROR message');
+    const formatedError = formatError(myError);
+    expect(formatedError).toEqual('This is the ERROR message');
+  });
+});
